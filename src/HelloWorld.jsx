97542e4861832d0aa79e5b165e26d1420fa9f829
swati4jha <swati4jha@gmail.com>
2018-02-09 12:33:54 -0500

    Update HelloWorld.jsx

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -6,45 +6,30 @@ import Canvas from './canvas.jsx'
 class App extends React.Component {
 	constructor(){
         super();
-        this.componentDidUpdate = this.componentDidUpdate.bind(this);
-        
-	}
+    }
 	
 	render(){
 		return (<div className="row abc">{
-			
 			this.props.users.map((users,i) =>{
 				var list = this.props.users.filter(user => user.table == i+1);
 				if(list.length>0){
 					console.log(i);
 					return (<div className="col-sm-6" key={users.id.toString()}><Canvas   
 					users={list}/></div>)
-				}
-				
+					}
 				}
 			)
 		}
 		</div>)
-		
-		
 	}
-
-
-
-componentDidUpdate(self,message) {
+}
+function showMsg(self,message) {
    
 	this.elementBox = self.getBoundingClientRect();
-	//this.elementHeight = self.node.clientHeight;
-	console.log(this.elementBox)
-	//var comp = React.createElement("a", {style: {position: "absolute", right: "50px", bottom: "100px"}}, {id: 'testPlace'},"ancdhck")
 	var comp = React.createElement('a', {style: {position:"absolute", zIndex:999, left: this.elementBox.x+40+"px", 
 	top: this.elementBox.y-40+ window.scrollY+"px",padding: "5px",borderRadius:"10px",border:"1px solid"}},message)
 	ReactDOM.render(comp, document.getElementById('test'));
 }
-  
-
-}
-
 const data=
             [   
                 {name:"Swati",
